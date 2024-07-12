4e958948392f438f6a345b474722bd2806ce1f52
mfbevan <matthewbevan00@gmail.com>
2024-07-12 16:32:18 +1000

    feat: ✨ ast + let statement parser

---
+++ parser/parser.go
@@ -0,0 +1,122 @@
+package parser
+
+import (
+	"bolt/ast"
+	"bolt/lexer"
+	"bolt/token"
+	"fmt"
+)
+
+type Parser struct {
+	l         *lexer.Lexer
+	curToken  token.Token
+	peekToken token.Token
+	errors    []string
+}
+
+// Create, initialize and return a new Parser instance
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
+	p.nextToken()
+	p.nextToken()
+	return p
+}
+
+// Advance the current and peek tokens
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+// Return the current program's errors
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
+}
+
+// Parse the input program
+//   - Create a new AST program
+//   - Iterate over the input tokens and parse each statement
+//   - Append each statement to the program
+//   - Return the program
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return program
+}
+
+// Attempt to parse an individual statement based on the current token type
+//   - If the current token is a LET token, parse a let statement
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+// Parse a let statement to ensure that it is well-formed
+//   - The statement must start with the token.LET token
+//   - The next token must be an identifier
+//   - The next token must be an assignment token
+//   - Skip over any tokens until we encounter a semicolon
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.curToken}
+
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	// TODO
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+// Check if the current token is of a given type
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+// Check if the next token is of a given type
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+// Assertion function to ensure that the next token is of a given type
+//   - If the next token is of the given type, consume it and return true
+//   - Otherwise, return false
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		p.peekError(t)
+		return false
+	}
+}
